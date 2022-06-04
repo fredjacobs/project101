746ee19efcd6757b18c0629bba5a73602ca36cf9
fredjacobs <thefredjacobs@gmail.com>
2022-06-04 20:32:04 +0200

    Added Menu Off Canvas

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,9 +1,42 @@
 <template>
   <div class="row toolbar py-2">
     <div class="col-md-4 text-white">Company Project</div>
+
     <div class="col-md-4 text-white">
-      <router-link to="/">HOME</router-link>
-      <router-link to="/projectslistview">Projects</router-link>
+      <!--Off Canvas #####################################-->
+
+      <button
+        class="btn btn-primary"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasRight"
+        aria-controls="offcanvasRight"
+      >
+        MENU
+      </button>
+
+      <div
+        class="offcanvas offcanvas-end"
+        tabindex="-1"
+        id="offcanvasRight"
+        aria-labelledby="offcanvasRightLabel"
+      >
+        <div class="offcanvas-header">
+          <h5 id="offcanvasRightLabel">Offcanvas right</h5>
+          <button
+            type="button"
+            class="btn-close text-reset"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body">
+          <router-link to="/">HOME</router-link>
+          <router-link to="/projectslistview">Projects</router-link>
+        </div>
+      </div>
+
+      <!--##################################-->
     </div>
     <div class="col-md-4 text-white">{{ name }}</div>
   </div>
