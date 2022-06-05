ce4869a904a3c487daeaf1d0a060f9c548fbd3c1
fredjacobs <thefredjacobs@gmail.com>
2022-06-05 17:23:57 +0200

    Add menu icon to toolbar

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -5,15 +5,22 @@
     <div class="col-md-4 text-white">
       <!--Off Canvas #####################################-->
 
-      <button
-        class="btn btn-primary"
-        type="button"
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        fill="currentColor"
+        class="bi bi-list toolbar-menu-icon"
+        viewBox="0 0 16 16"
         data-bs-toggle="offcanvas"
         data-bs-target="#offcanvasRight"
         aria-controls="offcanvasRight"
       >
-        MENU
-      </button>
+        <path
+          fill-rule="evenodd"
+          d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+        />
+      </svg>
 
       <div
         class="offcanvas offcanvas-end"
@@ -51,5 +58,9 @@ const name = ref("Fred");
 <style scoped>
 .toolbar {
   background-color: rgb(79, 79, 79);
+}
+
+.toolbar-menu-icon {
+  cursor: pointer;
 }
 </style>
